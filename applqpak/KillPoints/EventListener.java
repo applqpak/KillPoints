8f00ee1c0ee384a3f7a89c6b94a2499f50e79616
applqpak <applqpak@gmail.com>
2016-08-05 09:53:22 +1000

    Update EventListener.java

--- applqpak/KillPoints/EventListener.java
+++ applqpak/KillPoints/EventListener.java
@@ -2,7 +2,7 @@ package applqpak.KillPoints;
 
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.EventHandler;
-import cn.nukkit.event.entity.EntityDamageEvent;
+import cn.nukkit.event.player.PlayerDeathEvent;
 import cn.nukkit.event.entity.EntityDamageByEntityEvent;
 import cn.nukkit.utils.TextFormat;
 import cn.nukkit.Player;
@@ -21,22 +21,22 @@ public class EventListener implements Listener
 
   @EventHandler
 
-  public void onPlayerDeath(EntityDamageEvent event)
+  public void onPlayerDeath(PlayerDeathEvent event)
   {
 
-    if(event instanceof EntityDamageByEntityEvent && event.getEntity() instanceof Player && event.getDamager() instanceof Player)
+    if(event.getEntity().getLastDamageCause() instanceof EntityDamageByEntityEvent)
     {
 
       Player victim = event.getEntity();
 
-      Player damager = event.getDamager().getPlayer();
-
-      if(!(victim.isAlive()))
+      if(event.getEntity().getLastDamageCause().getDamager() instanceof Player)
       {
 
-        damager.sendMessage(TextFormat.GREEN + "You have earned +1 KillPoint(s).");
+        Player killer = event.getEntity().getLastDamageCause().getDamager();
 
-        this.plugin.config.set(damager.getName().toLowerCase(), this.plugin.config.getInt(damager.getName().toLowerCase()) + 1);
+        killer.sendMessage(TextFormat.GREEN + "You have earned +1 KillPoint(s).");
+
+        this.plugin.config.set(killer.getName().toLowerCase(), this.plugin.config.getInt(killer.getName().toLowerCase()) + 1);
 
         victim.sendMessage(TextFormat.RED + "You have lost -1 KillPoint(s).");
 

360a7ad263a65bc7c02fb100eff949faf9932cca
applqpak <applqpak@gmail.com>
2016-08-04 21:44:21 +1000

    Create Main.java

---
+++ applqpak/KillPoints/Main.java
@@ -0,0 +1,50 @@
+package applqpak.KillPoints;
+
+import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.command.CommandExecutor;
+import cn.nukkit.utils.Config;
+import cn.nukkit.utils.TextFormat;
+
+import applqpak.KillPoints.commands.kp;
+
+import java.io.File;
+
+public class Main extends PluginBase implements CommandExecutor
+{
+
+  public Config config;
+
+  @Override
+
+  public void onEnable()
+  {
+
+    this.getDataFolder().mkdirs();
+
+    if(!(new File(this.getDataFolder(), "config.yml").exists())
+    {
+
+      saveResource("config.yml");
+
+    }
+
+    this.config = config;
+
+    this.getServer().getPluginManager().registerEvents(new EventListener(this), this);
+
+    this.getServer().getCommandMap().register("killpoints", new kp(this));
+
+    this.getLogger().info(TextFormat.GREEN + "Enabled.");
+
+  }
+
+  @Override
+
+  public void onDisable()
+  {
+
+    this.getLogger().info(TextFormat.RED + "Disabled.");
+
+  }
+
+}
